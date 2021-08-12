baad453b92acbdfc22dafd193e1392c3aa09e592
Wenjie Wang <wang@wwenjie.org>
2021-08-12 13:52:14 -0400

    Add references to package docs

--- R/malc.R
+++ R/malc.R
@@ -1,5 +1,17 @@
 ##' Multi-Category Angle-Based Large-Margin Classifiers
 ##'
+##' The package malc provides implementations of multi-category angle-based
+##' large-margin classifiers for high-dimensional data.
+##'
+##' @references
+##'
+##' Zhang, C., & Liu, Y. (2014). Multicategory Angle-Based Large-Margin
+##' Classification. \emph{Biometrika}, 101(3), 625--640.
+##'
+##' Liu, Y., Zhang, H. H., & Wu, Y. (2011). Hard or soft classification?
+##' large-margin unified machines. \emph{Journal of the American Statistical
+##' Association}, 106(493), 166--177.
+##'
 ##' @importFrom Rcpp sourceCpp
 ##' @useDynLib malc
 ##'
