133eb255316bb9930fb655209e9f19904c0d1df9
Wenjie Wang <wenjie.wang@lilly.com>
2021-09-27 13:50:01 -0400

    Add R interface for the efficient-tuning method

--- R/logis_reg.R
+++ R/logis_reg.R
@@ -92,3 +92,51 @@ malc_logistic_path <- function(x, y, lambda = NULL, alpha = 1, nlambda = 100,
     ## return
     fit
 }
+
+
+malc_logistic_et <- function(x, y, lambda = NULL, alpha = 1, nlambda = 100,
+                             lambda_min_ratio = NULL, weight = NULL,
+                             intercept = TRUE, standardize = TRUE,
+                             max_iter = 50, rel_tol = 1e-3, pmin = 1e-5,
+                             verbose = FALSE, ...)
+{
+    Call <- match.call()
+    ## pre-process
+    if (! is.matrix(x)) {
+        x <- as.matrix(x)
+    }
+    cat_y <- cat2z(y)
+    if (is.null(lambda_min_ratio)) {
+        lambda_min_ratio <- if (nrow(x) < ncol(x)) 1e-4 else 1e-2
+    }
+    ## model fitting
+    fit <- rcpp_logistic_et(
+        x = x,
+        y = cat_y$y - 1L,
+        lambda = null2num0(lambda),
+        alpha = alpha,
+        nlambda = nlambda,
+        lambda_min_ratio = lambda_min_ratio,
+        weight = null2num0(weight),
+        intercept = intercept,
+        standardize = standardize,
+        max_iter = max_iter,
+        rel_tol = rel_tol,
+        pmin = pmin,
+        verbose = verbose
+    )
+    ## post-process
+    fit$category <- cat_y
+    fit$call <- Call
+    fit$intercept <- intercept
+    fit$control <- list(
+        standardize = standardize,
+        max_iter = max_iter,
+        rel_tol = rel_tol,
+        pmin = pmin,
+        verbose = verbose
+    )
+    class(fit) <- "malc_logistic_et"
+    ## return
+    fit
+}
