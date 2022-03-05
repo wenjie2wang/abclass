2027b32ea6470bb0fc485d7931d7be3a26c3c62c
Wenjie Wang <wang@wwenjie.org>
2022-03-04 21:28:17 -0500

    Add procedure interface for AbclassGroupLasso

---
+++ src/boost_glasso.cpp
@@ -0,0 +1,49 @@
+//
+// R package abclass developed by Wenjie Wang <wang@wwenjie.org>
+// Copyright (C) 2021-2022 Eli Lilly and Company
+//
+// This file is part of the R package abclass.
+//
+// The R package abclass is free software: You can redistribute it and/or
+// modify it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or any later
+// version (at your option). See the GNU General Public License at
+// <https://www.gnu.org/licenses/> for details.
+//
+// The R package abclass is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+//
+
+#include <RcppArmadillo.h>
+#include <abclass.h>
+#include "export-helpers.h"
+
+// [[Rcpp::export]]
+Rcpp::List rcpp_boost_glasso(
+    const arma::mat& x,
+    const arma::uvec& y,
+    const arma::vec& lambda,
+    const unsigned int nlambda,
+    const double lambda_min_ratio,
+    const arma::vec& group_weight,
+    const arma::vec& weight,
+    const bool intercept = true,
+    const bool standardize = true,
+    const unsigned int nfolds = 0,
+    const bool stratified_cv = true,
+    const unsigned int max_iter = 1e5,
+    const double rel_tol = 1e-4,
+    const bool varying_active_set = true,
+    const double inner_min = -3.0,
+    const unsigned int verbose = 0
+    )
+{
+    abclass::BoostNet object {
+        x, y, inner_min, intercept, standardize, weight
+    };
+    return abclass_glasso_fit(object, y,
+                              lambda, nlambda, lambda_min_ratio, group_weight,
+                              nfolds, stratified_cv, max_iter, rel_tol,
+                              varying_active_set, verbose);
+}
--- src/export-helpers.h
+++ src/export-helpers.h
@@ -47,3 +47,51 @@ Rcpp::List abclass_net_fit(
             )
         );
 }
+
+// for AbclassGroupLasso objects
+template <typename T>
+Rcpp::List abclass_glasso_fit(
+    T& object,
+    const arma::uvec& y,
+    const arma::vec& lambda,
+    const unsigned int nlambda,
+    const double lambda_min_ratio,
+    const arma::vec& group_weight,
+    const unsigned int nfolds = 0,
+    const bool stratified_cv = true,
+    const unsigned int max_iter = 1e4,
+    const double rel_tol = 1e-4,
+    const bool varying_active_set = true,
+    const unsigned int verbose = 0
+    )
+{
+    object.fit(lambda, nlambda, lambda_min_ratio, group_weight,
+               max_iter, rel_tol, varying_active_set, verbose);
+    Rcpp::NumericVector lambda_vec { abclass::arma2rvec(object.lambda_) };
+    if (nfolds > 0) {
+        arma::uvec strata;
+        if (stratified_cv) {
+            strata = y;
+        }
+        abclass::abclass_glasso_cv(object, nfolds, strata);
+    }
+    return Rcpp::List::create(
+        Rcpp::Named("coefficients") = object.coef_,
+        Rcpp::Named("weight") = abclass::arma2rvec(object.obs_weight_),
+        Rcpp::Named("cross_validation") = Rcpp::List::create(
+            Rcpp::Named("nfolds") = nfolds,
+            Rcpp::Named("stratified") = stratified_cv,
+            Rcpp::Named("cv_accuracy") = object.cv_accuracy_,
+            Rcpp::Named("cv_accuracy_mean") =
+            abclass::arma2rvec(object.cv_accuracy_mean_),
+            Rcpp::Named("cv_accuracy_sd") =
+            abclass::arma2rvec(object.cv_accuracy_sd_)
+            ),
+        Rcpp::Named("regularization") = Rcpp::List::create(
+            Rcpp::Named("lambda") = abclass::arma2rvec(lambda_vec),
+            Rcpp::Named("group_weight") =
+            abclass::arma2rvec(object.group_weight_),
+            Rcpp::Named("lambda_max") = object.lambda_max_
+            )
+        );
+}
