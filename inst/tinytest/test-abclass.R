0a1d76ed21ad69cc574a67bf6bcc593d944885ae
Wenjie Wang <wenjie.wang@lilly.com>
2022-08-30 14:48:47 -0400

    Add more testing examples

--- inst/tinytest/test-abclass.R
+++ inst/tinytest/test-abclass.R
@@ -54,16 +54,6 @@ expect_equal(dim(prob), c(ntest, k))
 expect_equivalent(dim(coef(model4, s = 5)), c(p + 1, k - 1))
 expect_error(predict(model4), "newx")
 
-## test sparse matrices
-if (requireNamespace("Matrix", quietly = TRUE)) {
-    model4 <- abclass(as(train_x, "sparseMatrix"),
-                      train_y, nlambda = 5, loss = "lum",
-                      epsilon = 1e-2, group_penalty = "scad")
-    sp_newx <- as(test_x, "sparseMatrix")
-    expect_equal(predict(model4, sp_newx, s = 5),
-                 predict(model4, test_x, s = 5))
-}
-
 ## test as.matrix
 model4 <- abclass(as.data.frame(train_x),
                   train_y, nlambda = 5,
@@ -72,15 +62,18 @@ model4 <- abclass(as.data.frame(train_x),
 expect_equal(predict(model4, as.data.frame(test_x), s = 5), pred4)
 
 ## quick tests for other options
+## grouped
 qset <- expand.grid(loss = c("logistic", "boost", "hinge-boost", "lum"),
-                    gpenalty = c("mcp", "scad"),
+                    gpenalty = c("lasso", "mcp", "scad"),
                     KEEP.OUT.ATTRS = FALSE,
                     stringsAsFactors = FALSE)
 for (k in seq_len(nrow(qset))) {
     qmodel <- abclass(train_x, train_y,
+                      lambda = 0.01,
                       loss = qset$loss[k],
                       group_penalty = qset$gpenalty[k])
     qpred <- predict(qmodel, test_x)
+    qprob <- predict(qmodel, test_x, type = "prob")
 }
 
 ## ungrouped penalty
@@ -90,8 +83,51 @@ qset <- expand.grid(loss = c("logistic", "boost", "hinge-boost", "lum"),
                     stringsAsFactors = FALSE)
 for (k in seq_len(nrow(qset))) {
     qmodel <- abclass(train_x, train_y,
+                      lambda = 0.01,
                       loss = qset$loss[k],
                       alpha = qset$alpha[k],
                       grouped = FALSE)
     qpred <- predict(qmodel, test_x)
+    qprob <- predict(qmodel, test_x, type = "prob")
+}
+
+## test sparse matrices
+if (requireNamespace("Matrix", quietly = TRUE)) {
+    sp_train_x <- as(train_x, "sparseMatrix")
+    sp_test_x <- as(test_x, "sparseMatrix")
+
+    sp_model <- abclass(sp_train_x, train_y, nlambda = 5, loss = "lum",
+                        epsilon = 1e-2, group_penalty = "lasso")
+    expect_equal(predict(sp_model, test_x, s = 5),
+                 predict(sp_model, sp_test_x, s = 5))
+
+    ## quick tests
+    ## grouped
+    qset <- expand.grid(loss = c("logistic", "boost", "hinge-boost", "lum"),
+                        gpenalty = c("lasso", "mcp", "scad"),
+                        KEEP.OUT.ATTRS = FALSE,
+                        stringsAsFactors = FALSE)
+    for (k in seq_len(nrow(qset))) {
+        qmodel <- abclass(sp_train_x, train_y,
+                          lambda = 0.01,
+                          loss = qset$loss[k],
+                          group_penalty = qset$gpenalty[k])
+        qpred <- predict(qmodel, sp_test_x)
+        qprob <- predict(qmodel, sp_test_x, type = "prob")
+    }
+
+    ## ungrouped penalty
+    qset <- expand.grid(loss = c("logistic", "boost", "hinge-boost", "lum"),
+                        alpha = c(0, 1),
+                        KEEP.OUT.ATTRS = FALSE,
+                        stringsAsFactors = FALSE)
+    for (k in seq_len(nrow(qset))) {
+        qmodel <- abclass(sp_train_x, train_y,
+                          lambda = 0.01,
+                          loss = qset$loss[k],
+                          alpha = qset$alpha[k],
+                          grouped = FALSE)
+        qpred <- predict(qmodel, sp_test_x)
+        qprob <- predict(qmodel, sp_test_x, type = "prob")
+    }
 }
