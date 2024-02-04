88b2ca6fd85e14c5cc6d4cc9f72c83ce9e5081c4
Wenjie Wang <wang@wwenjie.org>
2024-02-04 15:31:04 -0500

    Update BIC.supclass method

--- R/bic.R
+++ R/bic.R
@@ -17,21 +17,15 @@
 
 ##' @importFrom stats BIC
 ##' @export
-BIC.logistic_suplasso <- function(object, ...)
+BIC.supclass <- function(object, ...)
 {
     nll <- attr(object$coefficients, "negLogL")
+    if (is.null(nll)) {
+        return(NULL)
+    }
     k <- apply(object$coefficients, 3L, function(a) {
         sum(abs(a) > .Machine$double.eps)
     })
     nobs <- length(object$category$y)
     log(nobs) * k + 2 * nll * nobs
-}
-
-##' @export
-BIC.logistic_supscad <- BIC.logistic_suplasso
-
-##' @export
-BIC.supclass <- function(object, ...)
-{
-    NULL
 }
